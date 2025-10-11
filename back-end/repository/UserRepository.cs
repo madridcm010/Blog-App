b47b44f83d8de0b4493a50cbf2284494d64af3dc
madridcm010 <snowfallslower@gmail.com>
2025-10-11 00:00:03 -0400

    10/10

--- back-end/repository/UserRepository.cs
+++ back-end/repository/UserRepository.cs
@@ -1,11 +1,16 @@
 ﻿using back_end.data;
 using back_end.models;
 using back_end.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
 namespace back_end.repository
 {
     public class UserRepository
     {
-
+        private readonly ApplicationDbContext _context;
+        public UserRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
     }
 }
